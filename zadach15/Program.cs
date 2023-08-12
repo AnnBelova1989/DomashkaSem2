c45e3b089e13bf4578014fab9749d19dfcf2448a
AnnBelova <anutik-89@tut.by>
2023-08-13 01:46:37 +0300

    задача 15

--- zadach15/Program.cs
+++ zadach15/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int VvodChisla(string message)
+{
+    System.Console.WriteLine(message);
+    return int.Parse(Console.ReadLine());
+}
+
+bool Correct(int day)
+{
+    if (day > 0 && day < 8)
+    {
+        return true;
+    }
+    System.Console.WriteLine("no correct");
+    return false;
+}
+
+bool Vyhodnoy(int day)
+{
+    return (day > 5);
+}
+
+int day = VvodChisla("vvedi: ");
+
+if (Correct(day))
+{
+    if (Vyhodnoy(day))
+    {
+        System.Console.WriteLine("yes");
+    }
+    else
+    {
+        System.Console.WriteLine("no");
+    }
+}
+
+
+/*
+int day= VvodChisla("Введите день недели: ");
+if(day==6 || day==7)
+{
+    System.Console.WriteLine("yes");
+}
+else if(day>0 && day<6)
+{
+    System.Console.WriteLine("no");
+}
+else
+{
+System.Console.WriteLine("Такого дня не существует ");
+}
+*/
